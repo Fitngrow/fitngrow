c89227f6579c2b1f2d53dbbe00f5de1bc6156ec7
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-09 23:48:42 +0100

    Añadido index.html a la web

--- server.js
+++ server.js
@@ -14,12 +14,18 @@ var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+
 // routes =============================================
 
 //En este apartado, invocamos a distintos modulos, en los que nosotros vamos a ir añadiendo las rutas
 var apiroot = '/api/v1';
 require('./app/routes/cars')(app, apiroot);
 require('./app/routes/drivers')(app, apiroot);
+
+
+require('./app/routes/frontend')(app);
 
 // start app
 
