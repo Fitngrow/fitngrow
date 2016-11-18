444b4ef00cef00f8ff4e9b6b6289994ffd9a28a3
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-18 12:32:19 +0100

    No mostrar botones al cumplir condiciones

--- public/js/create-training.js
+++ public/js/create-training.js
@@ -3,11 +3,16 @@ angular.module("FitngrowApp")
         console.log("Welcome to Create training!");
 
 
+        $scope.hideStart = false;
+        $scope.hideEnd = true;
 
         $scope.startTraining = function(){
             $scope.start = new Date();
+            $scope.hideStart = true;
+            $scope.hideEnd = false;
         };
         $scope.endTraining = function(){
             $scope.end = new Date();
+            $scope.hideEnd = true;
         };
     });
