97d45bf0aa6911dc9a6926f701bbece22a59ff87
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-18 12:21:48 +0100

    Registrado comenzar y terminar

--- public/js/create-training.js
+++ public/js/create-training.js
@@ -2,4 +2,12 @@ angular.module("FitngrowApp")
     .controller("CreateTraining", function($scope, $http){
         console.log("Welcome to Create training!");
 
+
+
+        $scope.startTraining = function(){
+            $scope.start = new Date();
+        };
+        $scope.endTraining = function(){
+            $scope.end = new Date();
+        };
     });
