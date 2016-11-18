371bf9d4326a04b7b1f16b6d6b8b1baebd9c650c
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-18 13:14:15 +0100

    Finalizado crear entrenamiento

--- public/js/create-training.js
+++ public/js/create-training.js
@@ -3,10 +3,7 @@ angular.module("FitngrowApp")
         console.log("Welcome to Create training!");
 
 
-        $scope.newTraining = {};
-        $scope.hideStart = false;
-        $scope.hideEnd = true;
-        $scope.hideTrainingForm = true;
+        refresh();
 
         $scope.startTraining = function(){
             $scope.newTraining.start = new Date();
@@ -17,5 +14,56 @@ angular.module("FitngrowApp")
             $scope.newTraining.end = new Date();
             $scope.hideEnd = true;
             $scope.hideTrainingForm = false;
+
+            $scope.totalTime = getTotalTime()
         };
+        $scope.resetTraining = function(){
+            refresh()
+        };
+        $scope.saveTraining = function(){
+            var newTraining = $scope.newTraining;
+
+            $http.post("/api/v1/trainings", newTraining).success((e)=>{
+                refresh();
+            })
+
+        };
+
+        function refresh(){
+            $scope.newTraining = {
+                averageHeartRate: 0,
+                calories: 0,
+                distance: 0,
+                start: null,
+                end: null
+            };
+            $scope.hideStart = false;
+            $scope.hideEnd = true;
+            $scope.hideTrainingForm = true;
+            $scope.totalTime = null;
+        }
+
+        function getTotalTime(){
+            var start = $scope.newTraining.start;
+            var end = $scope.newTraining.end;
+            var diff = end-start
+            diff = new Date(diff)
+            var msec = diff.getMilliseconds()
+            var sec = diff.getSeconds()
+            var min = diff.getMinutes()
+            var hr = diff.getHours()-1
+            if (min < 10){
+                min = "0" + min
+            }
+            if (sec < 10){
+                sec = "0" + sec
+            }
+            if(msec < 10){
+                msec = "00" +msec
+            }
+            else if(msec < 100){
+                msec = "0" +msec
+            }
+            return hr + ":" + min + ":" + sec + ":" + msec
+        }
     });
