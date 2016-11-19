d31265049ef1e4b820ea7f0ac6c159ec8fc87f43
David López Chica <Fasgort@gmail.com>
2016-11-19 23:33:46 +0100

    Creación de una DB general para fitngrow. Llamada a los ficheros API actualizada. .gitignore actualizado.

--- server.js
+++ server.js
@@ -1,16 +1,54 @@
 // modules ===========================================
 
-var express    = require('express');
-var app        = express();
+var express = require('express');
 var bodyParser = require('body-parser');
+var dataStore = require("nedb");
+var path = require("path");
+var app = express();
 
 // configuration =====================================
 
 // config files
 
 // set our port
-
 var port = process.env.PORT || 8080;
+
+// DB creation
+var dbFileName = path.join(__dirname, 'db/');
+var db = {};
+
+db.achievements = new dataStore({
+    filename: dbFileName + "achievements.json",
+    autoload: true
+});
+
+db.prizes = new dataStore({
+    filename: dbFileName + "prizes.json",
+    autoload: true
+});
+
+db.records = new dataStore({
+    filename: dbFileName + "records.json",
+    autoload: true
+});
+
+db.routes = new dataStore({
+    filename: dbFileName + "routes.json",
+    autoload: true
+});
+
+db.trainings = new dataStore({
+    filename: dbFileName + "trainings.json",
+    autoload: true
+});
+
+db.users = new dataStore({
+    filename: dbFileName + "users.json",
+    autoload: true
+});
+
+console.log("DB initialized.");
+// End DB
 
 app.use(bodyParser.json());
 
@@ -22,16 +60,14 @@ app.use(express.static(__dirname + '/public'));
 //En este apartado, invocamos a distintos modulos, en los que nosotros vamos a ir añadiendo las rutas
 var apiroot = '/api/v1';
 
-require('./app/routes/achievements')(app, apiroot);
-require('./app/routes/prizes')(app, apiroot);
-require('./app/routes/records')(app, apiroot);
-require('./app/routes/routes')(app, apiroot);
-require('./app/routes/trainings')(app, apiroot);
+require('./app/routes/achievements')(app, apiroot, db);
+require('./app/routes/prizes')(app, apiroot, db);
+require('./app/routes/records')(app, apiroot, db);
+require('./app/routes/routes')(app, apiroot, db);
+require('./app/routes/trainings')(app, apiroot, db);
 
 // start app
-
 app.listen(port);
 
 // app running message
-
-console.log('Awesome server started on port ' + port);
+console.log('Server is ready. Running on port ' + port);
