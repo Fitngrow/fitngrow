8da930b2e4c630a80e798165647e9e68652c2769
David López Chica <Fasgort@gmail.com>
2016-11-22 23:37:55 +0100

    Merge branch 'dev'.

--- app/routes/prizes.js
+++ app/routes/prizes.js
@@ -4,119 +4,105 @@
  * @param apiroot ruta raiz para la api
  * @author Albert_FX91
  */
-module.exports = function(app, apiroot){
+module.exports = function(app, apiroot, db){
 
-    var prizes = [
-        {
-            _id: 1,
-            name: '100 monedas',
-            description: 'Has ganado un total de 100 monedas!',
-            code: '54E68S234J',
-            type: 'coins'
-        },
-        {
-            _id: 2,
-            name: '50 monedas',
-            description: 'Has ganado un total de 50 monedas!',
-            code: '43JJ98ZH3D',
-            type: 'coins'
-        },
-        {
-            _id: 3,
-            name: 'Fit&Grow shirt',
-            description: 'Has ganado una camiseta de fit&grow!',
-            code: 'FLMIJ91KS1',
-            type: 'shirt'
+var dataStore = require('nedb');
+
+db.prizes.find({}, (err, prizes) => {
+        if(prizes.length == 0){
+            /* db.prizes.insert({ meters: 0, sessions: 0, averageMeters: 0, calories : 0, totalTime : 0}) */
+            db.prizes.insert([{name: '100 monedas', description: 'Has ganado un total de 100 monedas!', code: '54E68S234J', type: 'coins' },
+                            {name: '50 monedas', description: 'Has ganado un total de 50 monedas!', code: '43JJ98ZH3D', type: 'coins' },
+                            {name: 'Fit&Grow shirt', description: 'Has ganado una camiseta de fit&grow!', code: 'FLMIJ91KS1', type: 'shirt' }])
+            console.log("DB is empty. Added default prizes.");
         }
-    ];
+    });
 
-    //Recibir todos los premios almacenados en el sistema
-    app.get(apiroot+'/prizes', function(req, res){
-        res.json(prizes);
+    // Recibir todos los premios almacenados en el sistema
+    app.get(apiroot+"/prizes",(req,res)=>{
+        db.prizes.find({},(err,prizes)=>{
+            if (err){
+                res.sendStatus(500);
+            }else{
+                res.send(prizes);
+            }
+        })
     });
 
     //Eliminar todos los premios almacenados en el sistema
-    app.delete(apiroot+'/prizes', function(req, res){
-        prizes = [];
-        res.sendStatus(200);
+    app.delete(apiroot+"/prizes",(req,res)=>{
+        var _id = req.params._id;
+
+        db.prizes.remove({},{multi : true},(err,numRemoved)=>{
+            if (err){
+                res.sendStatus(500);
+            }else{
+                res.sendStatus(200);
+            }
+        })
     });
 
-    //Añadir un nuevo premio al sistema
-    app.post(apiroot+'/prizes', function(req, res){
+    // Añadir un nuevo premio al sistema
+    app.post(apiroot+"/prizes",(req,res)=>{
         var prize = req.body;
-        prizes.push(prize);
-        res.sendStatus(201);
+
+        var _id = prize._id;
+        db.prizes.find({_id : _id},(err,prizes)=>{
+        if (prizes.length == 0){
+            db.prizes.insert(prize);
+            res.sendStatus(200);
+        }else{
+            res.sendStatus(409);
+        }
+    })
     });
 
-    //Recibir un premio concreto
-    app.get(apiroot+'/prizes/:code', function(req, res){
-        //Recogemos el codigo que vamos a capturar desde la URI con la nomenclatura :<nombre>
-        var code = req.params.code;
+    // Recibir un premio concreto
+    app.get(apiroot+"/prizes/:_id",(req,res)=>{
+        var _id = req.params._id;
 
-        //Buscamos el premio por el código
-        var prize = findPrizeByCode(code);
-
-        //En el caso que exista, lo devuelvo. En el caso contrario envío un código 404
-        (prize) ? res.json(prize) : res.sendStatus(404);
+        db.prizes.find({_id : _id},(err,prizes)=>{
+            if (err){
+                res.sendStatus(500);
+            }else{
+                if (prizes.length > 0)
+                    res.send(prizes[0]);
+                else
+                    res.sendStatus(404);
+            }   
+        })
     });
 
     //Eliminar un premio concreto
-    app.delete(apiroot+'/prizes/:code', function(req, res){
-        //Recogemos el codigo que vamos a capturar desde la URI con la nomenclatura :<nombre>
-        var code = req.params.code;
+    app.delete(apiroot+"/prizes/:_id",(req,res)=>{
+        var _id = req.params._id;
 
-        //Busco el índice del premio por el código para luego eliminarlo.
-        var index = findPrizeIndexByCode(code);
-
-        //Si el elemento existe, index será mayor que -1, por lo que con slice eliminamos ese elemento. En caso contrario enviamos 404
-        if (index > -1){
-            prizes.splice(index,1);
-            res.sendStatus(200);
-        }else{
-            res.sendStatus(404);
-        }
+        db.prizes.remove({_id : _id},{},(err,numRemoved)=>{
+            if (err){
+                res.sendStatus(500);
+            }else{
+                res.sendStatus(200);
+            }
+        })
     });
 
-    //Actualizamos un premio
-    app.put(apiroot+'/prizes/:code', function(req, res){
+    // Actualizar un premio
+    app.put(apiroot+'/prizes/:_id', function(req, res){ 
+        var _id = req.params._id;
         var prize = req.body;
 
-        //Recogemos el codigo que vamos a capturar desde la URI con la nomenclatura :<nombre>
-        var code = req.params.code;
-
-        //Busco el índice del premio por el código para luego eliminarlo.
-        var index = findPrizeIndexByCode(code);
-
-        //Si el elemento existe, index será mayor que -1, por lo que con actualizamos el elemento. En caso contrario enviamos 404
-        if (index > -1){
-            prizes[index] = prize;
-            res.sendStatus(200);
-        }else{
-            res.sendStatus(404);
+        if(_id != prize._id){
+            res.sendStatus(409);
+            return;
         }
+
+        db.prizes.update({_id : _id},prize,(err,numUpdate)=>{
+            if (err){
+                res.sendStatus(500);
+                    }else{
+                        res.sendStatus(200);
+                    }
+        })
     });
-
-    /**
-     * Busca un premio con el code pasado por parametro
-     * @param code
-     * @returns {*}
-     */
-    function findPrizeByCode(code){
-        //Realizamos la busqueda de un premio que tenga por código el recibido
-        return prizes.find( p => p.code == code);
-    }
-
-    /**
-     * Busca el indice que ocupa el premio con el codigo pasado por parametro
-     * @param code
-     * @returns {*}
-     */
-    function findPrizeIndexByCode(code){
-        //Realizamos la busqueda del premio
-        var prize = findPrizeByCode(code);
-
-        //Busco el índice donde está el elemento
-        return prizes.indexOf(prize);
-    }
 
 };
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,103 +1,106 @@
 
-module.exports = function(app, apiroot){
+module.exports = function (app, apiroot, db) {
 
-    //Algunas rutas
-    var route1 = {_id: 1, name: "Caminito del rey", type: "Ruta de naturaleza", subtype: "Senderismo", location: "Málaga", length: 7.7, time:5};
-    var route2 = {_id: 2, name: "Sevilla centro", type: "Ruta de ciudad", subtype: "Running", location: "Sevilla", length: 5, time:3};
-	var route3 = {_id: 3, name: "Reina Mercedes", type: "Ruta de ciudad", subtype: "Running", location: "Sevilla", length: 2, time:1};
-    var routes = [route1, route2, route3];
 
-    // GET. Devolver todas las rutas almacenadas
-    app.get(apiroot+'/routes', function(req, res){
-        //Se devuelven todas las rutas
-        res.json(routes);
-    });
-
-    // GET(id). Devolver una ruta según su id
-    app.get(apiroot+'/routes/:_id', function(req, res){
-        //Se recoge el id pasado
-        var _id = req.params._id;
-
-        //Se busca la ruta con ese id
-        var route = findRouteBy_id(_id);
-
-        //Si la ruta con ese id existe se devuelve la ruta y si la ruta con ese id no existe se envía un código 404 (no encontrado)
-        (route ? res.json(route) : res.sendStatus(404));
-    });
-
-    // POST. Añadir una nueva ruta
-    app.post(apiroot+'/routes', function(req, res){
-        //Se recogen los datos de la nueva ruta
-        var route = req.body;
-
-        //Se añade la ruta y se envía un código 201 (creado)
-        routes.push(route);
-        res.sendStatus(201);
-    });
-	
-	//PUT. Actualizar una ruta según su id
-    app.put(apiroot+'/routes/:_id', function(req, res){
-		// Se recoge el id pasado
-        var _id = req.params._id;
-		
-		// Se recogen los nuevos valores a actualizar de la ruta
-        var route = req.body;
-
-        // Se busca el índice de la ruta según el id pasado
-        var index = findRouteIndexBy_id(_id);
-
-        /* Si la ruta existe (valor de index mayor que -1) se actualiza la ruta de dicho índice y se envía el código 200 (OK).   
-           Si no existe se envía el código 404 (no encontrado) */
-        if (index > -1){
-            routes[index] = route;
-            res.sendStatus(200);
-        }else{
-            res.sendStatus(404);
+    db.routes.find({}, (err, routes) => {
+        if (routes.length == 0) {
+            db.routes.insert([
+                { name: "Caminito del rey", type: "Trekking", location: "Málaga", length: 7.7, time: 5 },
+                { name: "Sevilla centro", type: "Running", location: "Sevilla", length: 5, time: 3 },
+                { name: "Reina Mercedes", type: "Skating", location: "Sevilla", length: 2, time: 1 }
+            ])
+            console.log("DB is empty. Added default routes.");
         }
     });
 
-    //DELETE(id). Eliminar la ruta que corresponde al id pasado
-    app.delete(apiroot+'/routes/:_id', function(req, res){
-        // Se recoge el id pasado
+    // Recibir todas las rutas almacenadas en el sistema
+    app.get(apiroot + "/routes", (req, res) => {
+        db.routes.find({}, (err, routes) => {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                res.send(routes);
+            }
+        })
+    });
+
+    // Recibir una ruta concreta
+    app.get(apiroot + "/routes/:_id", (req, res) => {
         var _id = req.params._id;
 
-        // Se busca el índice de la ruta según el id pasado
-        var index = findRouteIndexBy_id(_id);
+        db.routes.find({ _id: _id }, (err, routes) => {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                if (routes.length > 0)
+                    res.send(routes[0]);
+                else
+                    res.sendStatus(404);
+            }
+        })
+    });
 
-        /* Si la ruta existe (valor de index mayor que -1) se borra la ruta de dicho índice y se envía el código 200 (OK).   
-           Si no existe se envía el código 404 (no encontrado) */
-        if (index > -1){
-            //splice(posicionAEliminar,NumItemsAEliminar)
-            routes.splice(index,1);
-            res.sendStatus(200);
-        }else{
-            res.sendStatus(404);
+    // Añadir una nueva ruta
+    app.post(apiroot + "/routes", (req, res) => {
+        var route = req.body;
+
+        var _id = route._id;
+        db.routes.find({ _id: _id }, (err, routes) => {
+            if (routes.length == 0) {
+                db.routes.insert(route);
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(409);
+            }
+        })
+    });
+
+    // Actualizar una ruta
+    app.put(apiroot + '/routes/:_id', function (req, res) {
+        var _id = req.params._id;
+        var route = req.body;
+
+        if (_id != route._id) {
+            res.sendStatus(409);
+            return;
         }
+
+        db.routes.update({ _id: _id }, route, (err, numUpdate) => {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(200);
+            }
+        })
     });
 
-    // DELETE. Eliminar todas las rutas
-    app.delete(apiroot+'/routes', function(req, res){
-        routes = [];
-        res.sendStatus(200);
+    // Eliminar una ruta
+    app.delete(apiroot + "/routes/:_id", (req, res) => {
+        var _id = req.params._id;
+
+        db.routes.remove({ _id: _id }, {}, (err, numRemoved) => {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(200);
+            }
+        })
     });
 
-    // Buscar una ruta pasando el id de dicha ruta por parámetro
-    function findRouteBy_id(_id){
-        //  Se busca y almacena aquella ruta que tenga el id pasado por parámetro
-        var route = routes.find( r => r._id == _id);
 
-       return route;
-    }
+    // Eliminar todos las rutas
+    app.delete(apiroot + "/routes", (req, res) => {
+        var _id = req.params._id;
 
-    // Buscar el índice que ocupa la ruta con el id pasado por parámetros
-    function findRouteIndexBy_id(_id){
-        //Se busca la ruta
-        var route = findRouteBy_id(_id);
+        db.routes.remove({}, { multi: true }, (err, numRemoved) => {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(200);
+            }
+        })
+    });
 
-        //Se busca el índice donde está dicha ruta
-        var index = routes.indexOf(route);
 
-        return index;
-    }
 
 };
