fc21ef9d0346a99efc17f66b6a8f3dbd258729d3
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-30 11:50:28 +0100

    Added example users

--- app/routes/users.js
+++ app/routes/users.js
@@ -10,6 +10,46 @@ var passport = require('passport');
 
 module.exports = function (app, apiroot, db) {
 
+    //Cargamos la DB, repoblando si está vacía
+    db.users.find({}, (err, users) => {
+        if (users.length == 0) {
+            db.users.insert([
+                {
+                    _id: "1",
+                    fullName: 'Full username 1',
+                    username: 'user1',
+                    password: 'user1',
+                    email: 'user1@gmail.com',
+                    birthdate: new Date("1991-06-02"),
+                    height: 1.86,
+                    weight: 80.0
+                },
+                {
+                    _id: "2",
+                    fullName: 'Full username 2',
+                    username: 'user2',
+                    password: 'user2',
+                    email: 'user2@gmail.com',
+                    birthdate: new Date("1996-03-12"),
+                    height: 1.60,
+                    weight: 60.0
+                },
+                {
+                    _id: "3",
+                    fullName: 'Linus Benedict Torvalds',
+                    username: 'Linux4Life',
+                    password: 'Linux4Life',
+                    email: 'Linus@gmail.com',
+                    birthdate: new Date("1969-12-28"),
+                    height: 1.73,
+                    weight: 73.0
+                }
+            ]);
+            console.log("DB is empty. Added default users.");
+        } else {
+            console.log("Loaded " + users.length + " users from the DB.");
+        }
+    });
 
     app.post(apiroot + '/users/login', passport.authenticate('local'), function(req, res){
         //If this function gets called, authentication was successful
