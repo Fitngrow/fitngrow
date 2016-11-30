78052c6ea22bd2c75f8bea41c88b8cd96c90c2e2
David López Chica <Fasgort@gmail.com>
2016-11-30 16:03:56 +0100

    Función backend para devolver logros conseguidos y sin conseguir. Uso de datos de usuario provisional.

--- public/js/listachievements-ctrl.js
+++ public/js/listachievements-ctrl.js
@@ -2,9 +2,39 @@ angular.module("FitngrowApp")
     .controller("ListAchievementsCtrl", function ($scope, $http) {
         console.log("List controller initialized");
 
+        // Info usuario provisional
+        var meters = 12350;
+        var seconds = 9700;
+
         function refresh() {
             $http.get("/api/v1/achievements").success(function (achievements) {
+
+                // Cálculo de logros
+                achievements.forEach(function (achievement) {
+                    switch (achievement.type) {
+
+                        case "meters":
+                            if (meters >= achievement.type_value) {
+                                achievement.achieved = true;
+                                achievement.url = achievement.url_achieved;
+                            }
+                            else achievement.achieved = false;
+                            break;
+
+                        case "seconds":
+                            if (seconds >= achievement.type_value) {
+                                achievement.achieved = true;
+                                achievement.url = achievement.url_achieved;
+                            }
+                            else achievement.achieved = false;
+                            break;
+
+                    }
+                })
+                // Cálculo de logros END
+
                 $scope.achievements = achievements;
+
             });
         }
 
