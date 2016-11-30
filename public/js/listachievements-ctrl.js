2007b2a32b56d04aa2cabbca645030ba92278f44
David López Chica <Fasgort@gmail.com>
2016-11-30 22:22:21 +0100

    Añadida una separación entre logros conseguidos y no conseguidos. Los logros conseguidos aparecen en primer lugar.

--- public/js/listachievements-ctrl.js
+++ public/js/listachievements-ctrl.js
@@ -10,30 +10,35 @@ angular.module("FitngrowApp")
             $http.get("/api/v1/achievements").success(function (achievements) {
 
                 // Cálculo de logros
+                var achieved_achievements = [];
+                var unachieved_achievements = [];
+
+
                 achievements.forEach(function (achievement) {
                     switch (achievement.type) {
 
                         case "meters":
                             if (meters >= achievement.type_value) {
-                                achievement.achieved = true;
                                 achievement.url = achievement.url_achieved;
+                                achieved_achievements.push(achievement);
                             }
-                            else achievement.achieved = false;
+                            else unachieved_achievements.push(achievement);
                             break;
 
                         case "seconds":
                             if (seconds >= achievement.type_value) {
-                                achievement.achieved = true;
                                 achievement.url = achievement.url_achieved;
+                                achieved_achievements.push(achievement);
                             }
-                            else achievement.achieved = false;
+                            else unachieved_achievements.push(achievement);
                             break;
 
                     }
                 })
                 // Cálculo de logros END
 
-                $scope.achievements = achievements;
+                $scope.achieved_achievements = achieved_achievements;
+                $scope.unachieved_achievements = unachieved_achievements;
 
             });
         }
