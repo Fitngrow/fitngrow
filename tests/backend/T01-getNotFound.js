85fa769c19f548d1d70fce0775739c2cd8973ee5
juagarfer4 <juanitogarciaman@gmail.com>
2016-12-03 22:06:56 +0100

    Realizados los primeros tests
    Se ha realizado un test en backend y otro en e2e

--- tests/backend/T01-getNotFound.js
+++ tests/backend/T01-getNotFound.js
@@ -2,13 +2,13 @@ var request = require("supertest");
 var should = require("should");
 var assert = require("assert");
 
-describe('Get of an unexisting data', function(){
-    it('should get a 404 status', function(){
+describe('Get of an unexisting data', function () {
+    it('should get a 404 status', function () {
         request('http://localhost:8080')
-        .get('/api/v1/records/xxxxxxxxxx')
-        .send()
-        .end(function(err,res){
-            res.status.should.be.equal(404);
-        });
+            .get('/api/v1/records/xxxxxxxxxx')
+            .send()
+            .end(function (err, res) {
+                res.status.should.be.equal(404);
+            });
     });
 });
--- tests/tests.js
+++ tests/tests.js
@@ -2,5 +2,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     chromeOnly: true,
     specs: [
+        'backend/T01-getNotFound.js',
+        'e2e/T01-loadData.js'
     ]
 };
