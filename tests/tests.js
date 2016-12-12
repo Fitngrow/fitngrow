89f1eafe65ba03257bdce9be1d4e446011d8fde6
juagarfer4 <juanitogarciaman@gmail.com>
2016-12-12 19:28:43 +0100

    Actualización de tests/tests.js
    Se ha hecho más eficiente la llamada a los tests

--- tests/tests.js
+++ tests/tests.js
@@ -2,10 +2,7 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     chromeOnly: true,
     specs: [
-        'backend/T01-nonExistingRecord.js',
-        'backend/T02-newUser.js',
-        'backend/T03-deleteAchievement.js',
-        'e2e/T01-getLogin.js',
-        'e2e/T02-listOfAchievements.js'
+        'backend/*.js',
+        'e2e/*.js'
     ]
 };
