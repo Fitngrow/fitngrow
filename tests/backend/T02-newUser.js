db51cf0693a80d5c799e1562920a9999121a62ad
juagarfer4 <juanitogarciaman@gmail.com>
2016-12-12 19:24:06 +0100

    Arreglo en T02-newUser.js
    Se ha arreglado el test de backend de creación de usuario para que el nuevo usuario sea insertado en la base de datos

--- tests/backend/T02-newUser.js
+++ tests/backend/T02-newUser.js
@@ -6,12 +6,10 @@ describe('Create a new user', function () {
     it('The request is correct, so we should get a 201 status', function () {
         request('http://localhost:8080')
             .post('/api/v1/users')
-            .type('form')
             .send({
                 fullName: 'testFullName', username: 'test', password: 'test', email: 'test@test.com',
                 birthdate: '1990-01-01', height: 1.91, weight: 81.1
             })
-            .set('Accept', /application\/json/)
             .end(function (err, res) {
                 res.status.should.be.equal(201);
             });
