71d2c31e92e337745d8a2ba3e068f5c77a67aadf
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-12-16 15:16:44 +0100

    Fixed get user id

--- public/js/listrecords-ctrl.js
+++ public/js/listrecords-ctrl.js
@@ -2,8 +2,9 @@ angular.module("FitngrowApp")
     .controller("ListRecordsCtrl", function ($scope, $http) {
         function refresh() {
             var user = $scope.currentUser;
-            var userId = user.params.userId;
+            var userId = user._id;
             $http.get("/api/v1/records/user/" + userId).then(function (response) {
+                console.log(response.data);
                 $scope.records = response.data;
             });
         }
