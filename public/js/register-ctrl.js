d000cb2adb781b6ae6d5cd99e3c5623451de7f1a
Miriam Romero Sánchez <miriam.r.s.l@gmail.com>
2016-12-26 21:35:43 +0100

    modificado pequeños detalles

--- public/js/register-ctrl.js
+++ public/js/register-ctrl.js
@@ -7,10 +7,10 @@ angular.module("FitngrowApp")
             var newUser = $scope.newUser;
             AuthService.register(newUser)
                 .then(function () {
-                  var username = AuthService.existsUserName();
-                    if(username){
+                    var username = AuthService.existsUserName();
+                    if (username) {
                         $scope.error = "Username already exists, please introduce other username";
-                    }else{                          
+                    } else {
                         $location.path("login")
                     }
                 })
