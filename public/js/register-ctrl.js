8666aa797952cdc387c460ed7038a665c1253d41
Miriam Romero Sánchez <miriam.r.s.l@gmail.com>
2016-12-28 20:15:48 +0100

    Transformación de la fecha del formulario de text a date

--- public/js/register-ctrl.js
+++ public/js/register-ctrl.js
@@ -4,6 +4,7 @@ angular.module("FitngrowApp")
         $scope.error = null;
 
         $scope.submit = function () {
+            $scope.newUser.birthdate = textToDate($scope.newUser.birthdate);
             var newUser = $scope.newUser;
             AuthService.register(newUser)
                 .then(function () {
@@ -11,9 +12,15 @@ angular.module("FitngrowApp")
                     if (username) {
                         $scope.error = "Username already exists, please introduce other username";
                     } else {
-                        $location.path("login")
+                        $location.path("login");
                     }
                 })
+        }
+
+        function textToDate(str) {           
+            var dt = str.substring(6, 10) + '-' + str.substring(3, 5) + '-' + str.substring(0, 2)           
+            var date1 = new Date(dt);
+            return date1;
         }
 
     });
