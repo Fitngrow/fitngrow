f5223c24fbdea37ff451419dc80f61301e77a691
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2017-01-13 19:47:00 +0100

    Fixed getRegister test

--- tests/e2e/T01-getRegister.js
+++ tests/e2e/T01-getRegister.js
@@ -4,11 +4,12 @@ var assert = require("assert");
 
 describe('Get the user register', function () {
     it('The user is registered, so we should get a 200 status', function () {
-        browser.get('http://localhost:8080/#/register');
+        var username = "test2";
 
+        browser.get('http://localhost:8080/#/register');
         element(by.css('[id="fullname"]')).sendKeys("test2FullName");
-        element(by.css('[id="username"]')).sendKeys("test2");
-        element(by.css('[id="password"]')).sendKeys("test2");
+        element(by.css('[id="username"]')).sendKeys(username);
+        element(by.css('[id="password"]')).sendKeys(username);
         element(by.css('[id="email"]')).sendKeys("email2@email2.com");
         element(by.css('[id="birthdate"]')).sendKeys("02/02/1992");
         element(by.css('[id="height"]')).sendKeys("1.92");
@@ -16,10 +17,11 @@ describe('Get the user register', function () {
         element(by.css('[id="option-button"]')).click();
 
         request('http://localhost:8080')
-            .get('/api/v1/users/service/status')
+            .get('/api/v1/users/service/existsUsername/'+username)
             .send()
             .end(function (err, res) {
                 res.status.should.be.equal(200);
+                res.body.status.should.be.equal(false);
             });
     });
 });
