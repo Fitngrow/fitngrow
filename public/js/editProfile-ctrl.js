3f12eb577af8c5f3c7c3a88275392590916e6c29
Miriam Romero Sánchez <miriam.r.s.l@gmail.com>
2017-01-15 14:30:15 +0100

    CSS mejorado

--- public/js/editProfile-ctrl.js
+++ public/js/editProfile-ctrl.js
@@ -1,7 +1,7 @@
 angular.module("FitngrowApp")
     .controller("EditProfileCtrl", function ($scope, $http, $location) {
 
-        function refresh() {           
+        function refresh() {
             var user = $scope.currentUser;
             var userId = user._id;
             $http.get("/api/v1/users/" + userId).then(function (response) {
@@ -12,15 +12,15 @@ angular.module("FitngrowApp")
 
         $scope.updateUser = function () {
             $scope.error = ""
-           
-           var editUser = $scope.profile;           
+
+            var editUser = $scope.profile;
 
             if ((editUser.fullName == null) || (editUser.email == null) || (editUser.birthdate == null) || (editUser.height == null) || (editUser.weight == null)) {
                 $scope.error = "Please make sure the fields are filled out correctly";
-            } else {                       
+            } else {
                 editUser.birthdate = textToDate(editUser.birthdate)
-                $http.put("/api/v1/users/" + editUser._id, editUser).then(function (response) {     
-                    $location.path("profile");               
+                $http.put("/api/v1/users/" + editUser._id, editUser).then(function (response) {
+                    $location.path("profile");
                 });
 
             }
